feae331f987589434ce171769830047fb6f70e6d
Sharky <114203428+UWU-Sharky@users.noreply.github.com>
2026-05-28 18:03:27 +0700

    UPD 9-th laboratory

--- lab09/project/app.py
+++ lab09/project/app.py
@@ -8,71 +8,40 @@ app = Flask(__name__)
 def ExpRV(lambd):
     return -math.log(1 - random.random()) / lambd
 
-def simulate_MM1_queue(lambd, mu, N, T_total):
+def simulate_MM1_queue(lambd, mu, T_total):
     current_time = 0
-    serving_client = 0
-    queue = 0
+    state = 0
+        
+    served_clients = 0
+    lost_clients = 0
     
-    waiting_times = []
-    state_durations = defaultdict(float) # Сколько времени система провела с 'k' клиентами внутри
-    
-    # Очередь, где хранятся моменты времени (timestamps) прихода клиентов
-    arrival_timestamps = deque()
-    
+    print(ExpRV(lambd))
+    print(ExpRV(mu))
+     
     while current_time < T_total:
-        time_next_client = ExpRV(lambd)
-        if serving_client > 0:
-            client_exit = ExpRV(mu * serving_client)
-        else:
-            client_exit = float('inf')
-          
-        if time_next_client < client_exit:
-            dt = time_next_client
-            is_arrival = True
-        else:
-            dt = client_exit
-            is_arrival = False
+        dt = ExpRV(lambd)
         
-        total_clients_now = serving_client + queue
-        state_durations[total_clients_now] += dt
-
-        current_time += dt
+        if state == 1:
+            delta = ExpRV(mu)
         
-        if is_arrival:
-            if serving_client < N:
-                serving_client += 1
-                waiting_times.append(0.0)  # Оператор свободен, время ожидания = 0
-            else:
-                queue += 1
-                arrival_timestamps.append(current_time)  # Запоминаем время входа в очередь
         else:
-            if queue == 0:
-                serving_client -= 1
-            else:
-                queue -= 1
-                # Клиент выходит из очереди к освободившемуся оператору
-                if arrival_timestamps:
-                    arrival_time = arrival_timestamps.popleft()
-                    wait_time = current_time - arrival_time
-                    waiting_times.append(wait_time)
-    
-    for k in range(len(state_durations)):
-        state_durations[k] /= T_total
-
-    avg_wait = sum(waiting_times) / len(waiting_times)
-    avg_clients = sum(k * v for k, v in state_durations.items()) / T_total
-    avg_queue = sum(k * v for k, v in state_durations.items() if k > 0) / T_total
-    rho = lambd / (mu * N) 
+            delta = float('inf')
+        
+        if dt < delta:
+            served_clients += 1
+            state = 1   
+            current_time += dt         
+        else:
+            lost_clients+=1
+            state = 0
+            current_time += delta
     
     return {
-        "states": dict(state_durations),
-        "waiting_times": waiting_times,
-        "statistics": {
-        "avg_wait": avg_wait,
-        "avg_clients": avg_clients,
-        "avg_queue": avg_queue,
-        "rho": rho,
-            }
+        "statistics":
+        {
+        "served": served_clients/(served_clients + lost_clients),
+        "lost": lost_clients/(served_clients + lost_clients),
+        }
     }
 
 
@@ -88,10 +57,9 @@ def simulate():
 
     lambd = float(data['lambda'])
     mu = float(data['mu'])
-    N = int(data['N'])
     T_total = float(data['T'])
 
-    result = simulate_MM1_queue(lambd, mu, N, T_total)
+    result = simulate_MM1_queue(lambd, mu, T_total)
 
     return jsonify(result)
 
--- lab09/project/static/script.js
+++ lab09/project/static/script.js
@@ -2,7 +2,6 @@ async function runSimulation() {
 
     const lambda = document.getElementById('lambda').value;
     const mu = document.getElementById('mu').value;
-    const N = document.getElementById('N').value;
     const T = document.getElementById('T').value;
 
     const response = await fetch('/simulate', {
@@ -16,103 +15,23 @@ async function runSimulation() {
         body: JSON.stringify({
             lambda,
             mu,
-            N,
             T
         })
     });
 
     const data = await response.json();
 
-    buildStateChart(data.states);
-
-    buildWaitChart(data.waiting_times);
-
     showStatistics(data.statistics);
 }
 
 
-let statesChart = null;
-let waitChart = null;
-
 
 function showStatistics(stats) {
 
-    document.getElementById('avg_wait').innerHTML =
-        `Среднее ожидание: ${stats.avg_wait.toFixed(3)}`;
+    document.getElementById('served').innerHTML =
+        `Вероятность обслужить заявку: ${stats.served.toFixed(3)}`;
 
-    document.getElementById('avg_clients').innerHTML =
-        `Среднее число клиентов: ${stats.avg_clients.toFixed(3)}`;
+    document.getElementById('lost').innerHTML =
+        `Вероятность потерять заявку: ${stats.lost.toFixed(3)}`;
 
-    document.getElementById('avg_queue').innerHTML =
-        `Средняя длина очереди: ${stats.avg_queue.toFixed(3)}`;
-
-    document.getElementById('rho').innerHTML =
-        `Коэффициент загрузки: ${stats.rho.toFixed(3)}`;
-}
-
-function buildStateChart(states) {
-
-    const labels = Object.keys(states);
-
-    const values = Object.values(states);
-
-    const ctx = document.getElementById('statesChart');
-
-    if (statesChart)
-        statesChart.destroy();
-
-    statesChart = new Chart(ctx, {
-
-        type: 'bar',
-
-        data: {
-            labels: labels,
-
-            datasets: [{
-                label: 'Распределение кол-ва клиентов по времени пребывания в системе',
-
-                data: values
-            }]
-        }
-    });
-}
-
-
-function buildWaitChart(waitingTimes) {
-
-    const freq = {};
-
-    waitingTimes.forEach(t => {
-
-        const rounded = Math.round(t);
-
-        if (!freq[rounded])
-            freq[rounded] = 0;
-
-        freq[rounded]++;
-    });
-
-    const labels = Object.keys(freq);
-
-    const values = Object.values(freq);
-
-    const ctx = document.getElementById('waitChart');
-
-    if (waitChart)
-        waitChart.destroy();
-
-    waitChart = new Chart(ctx, {
-
-        type: 'line',
-
-        data: {
-            labels: labels,
-
-            datasets: [{
-                label: 'Время ожидания клиентов в очереди',
-
-                data: values
-            }]
-        }
-    });
 }
